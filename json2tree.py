5ef137197459aa9693714cbf32e042585a7e6814
cibinjoseph <cibinjoseph92@gmail.com>
2020-04-18 15:04:08 +0530

    Add proper functions for parsing json data

--- json2tree.py
+++ json2tree.py
@@ -1,47 +1,56 @@
-#!/usr/bin/python3
-""" A python code to generate a tree for the JSON API links """
-import sys, os
-import json
-import requests
+""" A python code to generate a tree for JSON data"""
+from io import StringIO
 
-class Node:
-    def __init__(self, value=None, children=None):
-        if children is None:
-            children = []
-        self.value, self.children = value, children
+_branch_extend = "│  "
+_branch_mid    = "├─ "
+_branch_last   = '└─ '
+_spacing       = '   '
 
-def pprint_tree(node, file=None, _prefix="", _last=True):
-    print(_prefix, "└─ " if _last else "├─ ", node.value, sep="", file=file)
-    _prefix += "   " if _last else "│  "
-    child_count = len(node.children)
-    for i, child in enumerate(node.children):
-        _last = i == (child_count - 1)
-        pprint_tree(child, file, _prefix, _last)
+def _getHierarchy(jsonData, name='', file=None, _prefix="", _last=True):
+    """ Recursively parse json data to print data types """
+    # Handle object datatype
+    if isinstance(jsonData, dict):
+        name = name + ' (object)'
+        print(_prefix, _branch_last if _last else _branch_mid, \
+              name, sep="", file=file)
+        _prefix += _spacing if _last else _branch_extend
+        length = len(jsonData)
+        for i, key in enumerate(jsonData.keys()):
+            _last = i == (length - 1)
+            _getHierarchy(jsonData[key], '"' + key + '"', file, _prefix, _last)
+    # Handle array datatype
+    elif isinstance(jsonData, list):
+        name += ' (array)'
+        print(_prefix, _branch_last if _last else _branch_mid, \
+              name, sep="", file=file)
+        _prefix += _spacing if _last else _branch_extend
+        _getHierarchy(jsonData[0], '', file, _prefix, _last=True)
+    else:
+        # Handle string datatype
+        if isinstance(jsonData, str):
+            name = name + ' (string)'
+        # Handle number datatype
+        else:
+            name = name + ' (number)'
+        print(_prefix, \_branch_last if _last else _branch_mid, \
+              name, sep="", file=file)
 
-def printTree(field, _prefix=''):
-    global isFirstKey
-    if isinstance(field, dict):
-        print(_prefix + '|- (object)')
-        print('  ', end='')
-        for key in field.keys():
-            print(_prefix + key + ' ', end='')
-            printTree(field[key])
-    if isinstance(field, list):
-        print('(array)')
-        print('  ', end='')
-        printTree(field[0], _prefix='  ')
-    if isinstance(field, str):
-        print('(string)')
-        print('  ', end='')
-    if isinstance(field, int):
-        print('(integer)')
-        print('  ', end='')
+def json2tree(jsonData, file=None):
+    """ Output JSON data as tree to file or return as string """
+    if file == None:
+        messageFile = io.String()
+        _getHierarchy(jsonData, file=messageFile)
+        message = message.getValue()
+        messageFile.close()
+        return message
+    else:
+        _getHierarchy(jsonData, file=file)
 
-
-if __name__ == '__main__':
-    # f = open('data.json', 'r')
-    # jsonTest = json.load(f)
-    link = 'https://api.covid19india.org/state_district_wise.json'
-    jsonTest = requests.get(link).json()
-    printTree(jsonTest)
-
+def setSymbols(branch_extend="│  ", branch_mid="├─ ", branch_last='└─ '):
+    """ Override symbols for the tree structure """
+    global _branch_extend
+    global _branch_mid
+    global _branch_last
+    _branch_extend = branch_extend
+    _branch_mid    = branch_mid
+    _branch_last   = branch_last
